493f5eb06a6ece67b216fb6ce247d40e69807a38
Mauricio Farias da Silva <m.fariasdasilva@hotmail.com>
2025-12-18 02:52:09 +0100

    fix: corrigir import dayjs locale e adicionar integração com servidor

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import "./style/index.css";
-import dayjs from "dayjs";        // importar da node_modules
-import "dayjs/locale/de";          // se quiser em alemão
+import dayjs from "dayjs";
+import de from "dayjs/locale/de";
 import schedules from "./modules/schedules/load.js"
 import form from "./modules/form/submit.js"
 import loadPage from "./modules/load-page.js"
 
-dayjs.locale("de");               // define alemão globalmente
+dayjs.locale(de);
 
 console.log(dayjs().format("HH:mm")); // hora:minuto
 
